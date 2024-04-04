f4682e7379a3d46954221e9c205d82e1298d9ee2
Akash S <81205378+akash-selvaraj@users.noreply.github.com>
2024-04-04 22:15:48 +0530

    update

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -8,7 +8,7 @@ export default function AppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Minimal UI </title>
+        <title>BlueGreen Hive Dashboard</title>
       </Helmet>
 
       <AppView />
